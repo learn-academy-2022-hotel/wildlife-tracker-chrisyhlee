50e12bae2510c36e8ab04e0b1e99503c941b64a5
chrisyhlee <leechr8393@gmail.com>
2023-01-29 14:20:33 -0800

    completed most of story 4 stretch

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    muffin = Food.create(name:'Muffin', calories:400, protein:10)
+    expect(muffin).to be_valid
+  end
+  it 'is not valid without a name' do
+    muffin = Food.create(calories:400, protein:10)
+    expect(muffin.errors[:name]).to_not be_empty
+  end
+  it 'is not valid without a calorie amount' do
+    muffin = Food.create(name:'Muffin', protein:10)
+    expect(muffin.errors[:calories]).to_not be_empty
+  end
+  it 'is not valid without a protein amount' do
+    muffin = Food.create(name:'Muffin', calories:400)
+    expect(muffin.errors[:protein]).to_not be_empty
+  end
+  it 'is not valid if calories match protein' do
+    muffin = Food.create(name:'Muffin', calories:400, protein:400)
+    expect(muffin.errors[:protein]).to_not be_empty
+  end
+  it 'is not valid if name is not unique' do
+    muffin = Food.create(name:'Muffin', calories:400, protein:20)
+    bagel = Food.create(name:'Muffin', calories:400, protein:10)
+    expect(bagel.errors[:name]).to_not be_empty
+  end
+  it 'will return a 422 error if validations run into errors' do
+    muffin = Food.create(name:'Muffin', calories:100)
+    muffin.save
+    expect(muffin.errors[:protein]).to_not be_empty
+  end
 end
